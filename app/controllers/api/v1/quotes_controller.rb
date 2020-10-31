7a02494b958678bca7a11b4c5b678956ee8d3637
Shamel Lakin <christlife4u@gmail.com>
2020-10-31 15:43:33 -0400

    building CRUD

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::QuotesController < ApplicationController
+
+    def index 
+        quotes = Quote.all
+        render json: quotes
+    end 
+
+    def create 
+        quote = Quote.new(quote_params)
+    end 
+
+    private 
+    
+    def quote_params
+    end 
 end
