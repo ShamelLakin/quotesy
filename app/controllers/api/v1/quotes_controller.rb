b7aceca4a4a7fa02dfec53f62493d1f457d5e8c0
Shamel Lakin <christlife4u@gmail.com>
2020-11-01 11:04:17 -0500

    contoller create route and strong params

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -7,10 +7,16 @@ class Api::V1::QuotesController < ApplicationController
 
     def create 
         quote = Quote.new(quote_params)
+        if quote.save
+            render json: quote, status: :accepted
+        else
+            renedr json: {errors: quote.errors.full_messages}, status: :unprocessible_entity
+        end
     end 
 
     private 
-    
+
     def quote_params
+        params.requir(:quote).permit(:quote, :author, :tag, :category_id)
     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-    resources :quotes, only: [:index]
+    resources :quotes, only: [:index, :create]
     end
   end
 end
