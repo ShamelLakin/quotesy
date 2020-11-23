583f9740eb294ff6cd0cb650e15b758cf39177d1
Shamel Lakin <christlife4u@gmail.com>
2020-11-23 12:16:31 -0500

    delete fix

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -15,6 +15,21 @@ class Api::V1::QuotesController < ApplicationController
         end
     end 
 
+    def destroy
+        quote = Quote.find(params[:id])
+        if quote
+            quote.destroy
+            render json: {
+                quote: QuoteSerializer.new(quote).serializable_hash, 
+                status: :accepted
+            }
+        else
+            render json: {
+                message: "Couldn't find a quote with that ID"
+            }
+        end
+    end
+
     private 
 
     def quote_params
